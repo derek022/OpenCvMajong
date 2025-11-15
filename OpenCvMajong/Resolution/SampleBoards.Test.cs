2bb79e4911fb6d23d10a23c0f47706402b9104d7
J.C. <1004314574@qq.com>
2025-11-15 17:50:17 +0800

    添加复杂数据

--- OpenCvMajong/Resolution/SampleBoards.Test.cs
+++ OpenCvMajong/Resolution/SampleBoards.Test.cs
@@ -6,89 +6,61 @@ public partial class SampleBoards
 {
     public class TestData
     {
-         public static Cards[,] Test1 = new Cards[8, 6]
-    {
+        public static Cards[,] Test1 = new Cards[8, 6]
         {
-            Cards.Zero, Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-    };
+            {
+                Cards.Zero, Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+        };
 
-    public static Cards[,] TestDead = new Cards[8, 6]
-    {
+        public static Cards[,] TestDead = new Cards[8, 6]
         {
-            Cards.OneBmb, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.OneBmb, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-        {
-            Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
-        },
-    };
-    
-    public static Cards[,] Test2 = new Cards[8, 6]
-    {
-        {
-            Cards.WestWind, Cards.TwoBmb, Cards.EastWind, Cards.GreenDrg, Cards.SixDots, Cards.SixRank,
-        },
-        {
-            Cards.NorthWind, Cards.SevenRank, Cards.TwoBmb, Cards.NorthWind, Cards.SixRank, Cards.SixDots,
-        },
-        {
-            Cards.WestWind, Cards.SixRank, Cards.SevenRank, Cards.SevenBmb, Cards.WhiteDrg, Cards.NorthWind,
-        },
-        {
-            Cards.SixRank, Cards.WestWind, Cards.Zero, Cards.SixRank, Cards.ThreeDots, Cards.WhiteDrg,
-        },
-        {
-            Cards.OneBmb, Cards.SevenBmb, Cards.Zero, Cards.WestWind, Cards.TwoBmb, Cards.EastWind,
-        },
-        {
-            Cards.ThreeDots, Cards.EastWind, Cards.SixDots, Cards.SixDots, Cards.SixRank, Cards.WhiteDrg,
-        },
-        {
-            Cards.OneBmb, Cards.EastWind, Cards.SevenBmb, Cards.NorthWind, Cards.GreenDrg, Cards.WhiteDrg,
-        },
-        {
-            Cards.GreenDrg, Cards.OneBmb, Cards.SevenBmb, Cards.OneBmb, Cards.TwoBmb, Cards.GreenDrg,
-        },
-    };
+            {
+                Cards.OneBmb, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.OneBmb, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.WestWind, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+            {
+                Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero, Cards.Zero,
+            },
+        };
     }
-    
+
 }
--- OpenCvMajong/Resolution/SampleBoards.cs
+++ OpenCvMajong/Resolution/SampleBoards.cs
@@ -71,4 +71,58 @@ public partial class SampleBoards
             Cards.ThreeRank, Cards.EightRank, Cards.TwoRank, Cards.ThreeDots, Cards.WestWind, Cards.NorthWind, Cards.NineBmb, Cards.FiveRank, Cards.SevenDots, Cards.EastWind
         },
     };
+
+
+    public static Cards[,] ExpertBoard4 = new Cards[12, 10]
+    {
+        {
+            Cards.OneRank, Cards.SixBmb, Cards.FourBmb, Cards.Summer, Cards.EightDots, Cards.FourDots, Cards.SevenDots,
+            Cards.SevenBmb, Cards.EightBmb, Cards.SevenDots,
+        },
+        {
+            Cards.Summer, Cards.FiveDots, Cards.Orchid, Cards.ThreeDots, Cards.SixDots, Cards.FourDots, Cards.NorthWind,
+            Cards.WestWind, Cards.FiveBmb, Cards.EightDots,
+        },
+        {
+            Cards.FiveBmb, Cards.FiveBmb, Cards.SouthWind, Cards.SevenRank, Cards.NineBmb, Cards.WhiteDrg,
+            Cards.FiveRank, Cards.EastWind, Cards.Chrysan, Cards.SixDots,
+        },
+        {
+            Cards.FourRank, Cards.GreenDrg, Cards.ThreeRank, Cards.OneRank, Cards.FiveBmb, Cards.WestWind,
+            Cards.FiveRank, Cards.Orchid, Cards.SouthWind, Cards.SevenRank,
+        },
+        {
+            Cards.SevenRank, Cards.OneDots, Cards.ThreeRank, Cards.WhiteDrg, Cards.EastWind, Cards.OneRank,
+            Cards.OneDots, Cards.FourRank, Cards.WestWind, Cards.FiveDots,
+        },
+        {
+            Cards.ThreeRank, Cards.ThreeDots, Cards.SevenBmb, Cards.GreenDrg, Cards.OneDots, Cards.Chrysan,
+            Cards.FourRank, Cards.Autumn, Cards.SouthWind, Cards.Orchid,
+        },
+        {
+            Cards.SixDots, Cards.FiveRank, Cards.ThreeDots, Cards.NineBmb, Cards.NineRank, Cards.TwoDots,
+            Cards.EightDots, Cards.GreenDrg, Cards.FourBmb, Cards.NineBmb,
+        },
+        {
+            Cards.Chrysan, Cards.ThreeRank, Cards.OneRank, Cards.FiveDots, Cards.FourRank, Cards.SevenRank,
+            Cards.EightBmb, Cards.NorthWind, Cards.SixBmb, Cards.NineRank,
+        },
+        {
+            Cards.EightBmb, Cards.SixDots, Cards.OneDots, Cards.EastWind, Cards.WestWind, Cards.FourBmb,
+            Cards.Autumn, Cards.NorthWind, Cards.NineRank, Cards.TwoDots,
+        },
+        {
+            Cards.SevenBmb, Cards.Autumn, Cards.EightBmb, Cards.SevenBmb, Cards.SevenDots, Cards.FourBmb,
+            Cards.EastWind, Cards.GreenDrg, Cards.ThreeDots, Cards.NorthWind,
+        },
+        {
+            Cards.SevenDots, Cards.Autumn, Cards.WhiteDrg, Cards.Summer, Cards.Orchid, Cards.SixBmb,
+            Cards.TwoDots, Cards.SixBmb, Cards.FourDots, Cards.FiveDots,
+        },
+        {
+            Cards.WhiteDrg, Cards.Chrysan, Cards.NineRank, Cards.Summer, Cards.TwoDots, Cards.FiveRank,
+            Cards.SouthWind, Cards.NineBmb, Cards.FourDots, Cards.EightDots,
+        }
+    };
+
 }
